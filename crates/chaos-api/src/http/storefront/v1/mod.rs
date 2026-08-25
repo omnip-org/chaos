aecb7286fc3c795b7d3dc02ec2ca6751bf0faf90
Sricor <78024433+Sricor@users.noreply.github.com>
2026-08-25 19:56:22 +0800

    chore: update

--- crates/chaos-api/src/http/storefront/v1/mod.rs
+++ crates/chaos-api/src/http/storefront/v1/mod.rs
@@ -9,7 +9,7 @@ mod carts;
 mod collections;
 mod orders;
 mod products;
-mod shopper_sessions;
+mod shopper;
 mod webhooks;
 
 pub(crate) fn routes() -> Router<ApiState> {
@@ -18,7 +18,7 @@ pub(crate) fn routes() -> Router<ApiState> {
     Router::new()
         .merge(products::routes())
         .merge(collections::routes())
-        .merge(shopper_sessions::routes())
+        .merge(shopper::routes())
         .merge(carts::routes())
         .merge(orders::routes())
         .merge(analytics::routes())
